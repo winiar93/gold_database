6c270bc1eaf69510f2f31052401b35470856e184
Mike <winiar93@gmail.com>
2021-04-24 18:40:21 +0200

    3function with ability to run from console

--- scraper_db_gold_price/soup_scraper/beautiful_soup_scraper.py
+++ scraper_db_gold_price/soup_scraper/beautiful_soup_scraper.py
@@ -4,7 +4,10 @@ import requests
 import lxml
 from datetime import date
 import time
-
+import matplotlib.pyplot as plt
+import csv
+import re
+import ast
 
 def get_gold_price_from_web():
 
@@ -12,14 +15,27 @@ def get_gold_price_from_web():
     r = requests.get('https://www.coininvest.com/pl/wykresy/kurs-zlota/')
     soup = bs4.BeautifulSoup(r.text, 'xml')
     price = soup.find('div', {'class': 'live_metal_prices_li_in'}).find('span').text
-    return {today.strftime("%d/%m/%y"): price}
+    return {today.strftime("%d/%m/%y"): ast.literal_eval((price.replace('zł', '')).replace(",","."))}
 
 
 def get_gold_price_all_time():
 
     while True:
         print(get_gold_price_from_web())
-        time.sleep(3)
+        time.sleep(4)
 
 
-print(get_gold_price_all_time())
+def collect_gold_data():
+
+    while True:
+        with open('data_gold.csv', 'a') as goldfile:
+            goldfile.write(str(get_gold_price_from_web())+"\n")
+        print(get_gold_price_from_web())
+        time.sleep(4)
+
+if __name__ == '__main__':
+    get_gold_price_from_web()
+    get_gold_price_all_time()
+    collect_gold_data()
+
+#print(collect_gold_data())
