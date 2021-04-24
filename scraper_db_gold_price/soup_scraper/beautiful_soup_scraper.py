eb0d60ba7715bbb46b66c78c5b6564ba2999edad
Mike <winiar93@gmail.com>
2021-04-24 17:51:15 +0200

    two function added

--- scraper_db_gold_price/soup_scraper/beautiful_soup_scraper.py
+++ scraper_db_gold_price/soup_scraper/beautiful_soup_scraper.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import requests
 import lxml
 from datetime import date
-
+import time
 
 
 def get_gold_price_from_web():
@@ -12,7 +12,14 @@ def get_gold_price_from_web():
     r = requests.get('https://www.coininvest.com/pl/wykresy/kurs-zlota/')
     soup = bs4.BeautifulSoup(r.text, 'xml')
     price = soup.find('div', {'class': 'live_metal_prices_li_in'}).find('span').text
-    return price, today.strftime("%d/%m/%y")
+    return {today.strftime("%d/%m/%y"): price}
 
 
-print(get_gold_price_from_web())
+def get_gold_price_all_time():
+
+    while True:
+        print(get_gold_price_from_web())
+        time.sleep(3)
+
+
+print(get_gold_price_all_time())
