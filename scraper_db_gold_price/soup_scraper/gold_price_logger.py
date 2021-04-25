72c9793fc986b1398f2f33d7cfe6e36e30e56d89
Mike <winiar93@gmail.com>
2021-04-25 22:06:25 +0200

    add plot_graph.py script

--- scraper_db_gold_price/soup_scraper/gold_price_logger.py
+++ scraper_db_gold_price/soup_scraper/gold_price_logger.py
@@ -11,30 +11,28 @@ import csv
 import re
 import ast
 
+
 def start_loger():
+    counter = 0
+
+    while True:
+        r = requests.get('https://www.coininvest.com/pl/wykresy/kurs-zlota/')
+        soup = bs4.BeautifulSoup(r.text, 'xml')
+        now = datetime.now()
+        price = soup.find('div', {'class': 'live_metal_prices_li_in'}).find('span').text
+
+        with open("output_gold.csv", "a") as text_file:
+            text_file.write(str(counter))
+            text_file.write(",")
+            text_file.write(str(now.strftime("%m/%d/%Y/ %H:%M:%S")))
+            text_file.write(",")
+            text_file.write(str(ast.literal_eval((price.replace('zł', '')).replace(",", "."))) + "\n")
+
+        counter += 1
+        time.sleep(1)
+        text_file.close()
+        print({now.strftime("%m/%d/%Y, %H:%M:%S"): ast.literal_eval((price.replace('zł', '')).replace(",", "."))})
 
 
-        counter = 0
-
-        while True:
-
-                r = requests.get('https://www.coininvest.com/pl/wykresy/kurs-zlota/')
-                soup = bs4.BeautifulSoup(r.text, 'xml')
-                now = datetime.now()
-                price = soup.find('div', {'class': 'live_metal_prices_li_in'}).find('span').text
-
-                with open("output_gold.csv", "a") as text_file:
-                        text_file.write(str(counter))
-                        text_file.write(",")
-                        text_file.write(str(now.strftime("%m/%d/%Y/ %H:%M:%S")))
-                        text_file.write(",")
-                        text_file.write(str(ast.literal_eval((price.replace('zł', '')).replace(",", ".")))+"\n")
-
-                counter += 1
-                time.sleep(1)
-                text_file.close()
-                print({now.strftime("%m/%d/%Y, %H:%M:%S"): ast.literal_eval((price.replace('zł', '')).replace(",", "."))})
-
-
-start_loger()
-
+if __name__ == '__main__':
+    start_loger()
--- scraper_db_gold_price/soup_scraper/plot_graph.py
+++ scraper_db_gold_price/soup_scraper/plot_graph.py
@@ -1,12 +1,33 @@
 import matplotlib.pyplot as plt
 import os.path
+import csv
 
-# def plot_price():
-print(sum(1 for line in open('Output_gold.csv')))
+def gold_price_plot():
+
+    if os.path.isfile('Output_gold.csv'):
+        print("File exist")
+    else:
+        print("File not exist")
+    print("number of lines in file :")
+    print(sum(1 for line in open('Output_gold.csv')))
+
+    x = []
+    y = []
+
+    with open('Output_gold.csv','r') as sales_csv:
+        plots = csv.reader(sales_csv, delimiter=',')
+        for row in plots:
+            x.append(row[1])
+            y.append(row[2])
+
+    plt.plot(x,y, label='Loaded from file!')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.xticks(rotation=90)
+    plt.show()
 
 
+if __name__ == '__main__':
+    gold_price_plot()
 
-if os.path.isfile('Output_gold.csv'):
-    print ("File exist")
-else:
-    print ("File not exist")
+
