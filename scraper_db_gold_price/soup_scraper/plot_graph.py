a20a287d28918318baa24a6e4bd3a5fb49155a99
Mike <winiar93@gmail.com>
2021-04-25 22:09:33 +0200

    add plot_graph

--- scraper_db_gold_price/soup_scraper/plot_graph.py
+++ scraper_db_gold_price/soup_scraper/plot_graph.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import os.path
 import csv
 
+
 def gold_price_plot():
 
     if os.path.isfile('Output_gold.csv'):
@@ -14,13 +15,13 @@ def gold_price_plot():
     x = []
     y = []
 
-    with open('Output_gold.csv','r') as sales_csv:
+    with open('Output_gold.csv', 'r') as sales_csv:
         plots = csv.reader(sales_csv, delimiter=',')
         for row in plots:
             x.append(row[1])
             y.append(row[2])
 
-    plt.plot(x,y, label='Loaded from file!')
+    plt.plot(x, y, label='Loaded from file!')
     plt.xlabel('x')
     plt.ylabel('y')
     plt.xticks(rotation=90)
@@ -29,5 +30,4 @@ def gold_price_plot():
 
 if __name__ == '__main__':
     gold_price_plot()
-
 
