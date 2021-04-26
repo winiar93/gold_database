646f4d4f3fd22e5ae33ef4729616eaeba2cf4816
Mike <winiar93@gmail.com>
2021-04-26 16:01:22 +0200

    auto logging database script correction

--- scraper_db_gold_price/sql_data_base/first_module.py
+++
@@ -1,3 +0,0 @@
-print('first_module.py')
-if __name__ == '__main__':
-    print('zaimportowano pierwszy modul')
--- scraper_db_gold_price/sql_data_base/second_module.py
+++
@@ -1,4 +0,0 @@
-import first_module
-print('Hello from second_module.py')
-if __name__ == '__main__':
-    print('Hello from main method of second_module.py')
