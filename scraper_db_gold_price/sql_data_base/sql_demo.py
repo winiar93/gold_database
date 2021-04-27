3e58cf2899834551dba807d268fbfb38633a9d74
Mike <winiar93@gmail.com>
2021-04-27 09:56:26 +0200

    sql_demo have sql select operation

--- scraper_db_gold_price/sql_data_base/sql_demo.py
+++ scraper_db_gold_price/sql_data_base/sql_demo.py
@@ -3,13 +3,13 @@ import time
 from datetime import datetime
 from scraper_db_gold_price.soup_scraper.beautiful_soup_scraper import get_gold_price_from_web
 
+
 def connect_to_database():
     '''IF there is no database, sqlite create new one'''
     connection = sqlite3.connect('gold.db')
 
 
 def add_table():
-
     connection = sqlite3.connect('gold.db')
     c = connection.cursor()
     c.execute("""CREATE TABLE gold_price (
@@ -28,6 +28,7 @@ def insert_into_table():
     connection.commit()
     connection.close()
 
+
 def dynamic_data_insert():
     while True:
         connection = sqlite3.connect('gold.db')
@@ -36,31 +37,45 @@ def dynamic_data_insert():
         now.strftime("%m/%d/%Y, %H:%M:%S")
         take_date = str(now.strftime("%m/%d/%Y %H:%M:%S"))
         value = 2000
-        #get_gold_price_from_web()[1]
+        # get_gold_price_from_web()[1]
         c.execute("insert into gold_price (ID, date, price) values (NULL, ?, ?)",
-                 (get_gold_price_from_web()[0], get_gold_price_from_web()[1]))
+                  (get_gold_price_from_web()[0], get_gold_price_from_web()[1]))
 
         connection.commit()
+        c.execute("SELECT * FROM gold_price ORDER BY ID DESC LIMIT 1")
+        result = c.fetchone()
+        print("Added values to gold_price table : ", str(result))
         connection.close()
-        print()
         time.sleep(3)
 
+x = 'SELECT * FROM gold_price'
 
+class Read_data_from_db:
 
+    def __init__(self, connection=sqlite3.connect('gold.db')):
+        self.connection = connection
 
-def select_operation():
+    def select_operation(self):
+        c = self.connection.cursor()
+        c.execute("SELECT * FROM gold_price")
+        print(c.fetchall())
+        self.connection.commit()
+        self.connection.close()
 
-    connection = sqlite3.connect('gold.db')
-    c = connection.cursor()
-    c.execute("SELECT * FROM gold_price")
-    print(c.fetchall())
-    connection.commit()
-    connection.close()
-
+    def read_any_information(self):
+        c = self.connection.cursor()
+        sql_select = input("Type sql operation : ")
+        c.execute(sql_select)
+        print(c.fetchall())
+        self.connection.commit()
+        self.connection.close()
 
 # if __name__ == '__main__':
-select_operation()
+# select_operation()
 # connect_to_database()
 # add_table()
-#insert_into_table()
-#dynamic_data_insert()
+# insert_into_table()
+grab_data_from_db = Read_data_from_db()
+
+#grab_data_from_db.select_operation()
+grab_data_from_db.read_any_information()
